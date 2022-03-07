5132d6925f637b27f06995b4bc6328dc168dfa3d
Bekmaganbetov Zhanbolat <180107180@stu.sdu.edu.kz>
2022-03-07 23:30:41 +0600

    build: init conf for swagger, rest_framework

--- Project/backend/diplom/games_platform/core/settings/base.py
+++ Project/backend/diplom/games_platform/core/settings/base.py
@@ -30,6 +30,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'rest_framework',
+    'rest_framework_swagger',
     'users',
     'authentication',
 ]
