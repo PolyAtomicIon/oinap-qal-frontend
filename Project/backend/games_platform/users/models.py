a01ddb38770b646985e2438df6403656ed7ff80f
Yerlan Negmetulla <yerlan.negmetulla@uco.kz>
2022-03-12 22:55:29 +0600

    Added custom user manager

--- Project/backend/games_platform/users/models.py
+++ Project/backend/games_platform/users/models.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.db import models
 
-# Create your models here.
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, username, password, role=choices["GAMER"]):
+        if not email:
+            raise ValueError("Users must have email address")
+        if not username:
+            raise ValueError("Users must have username")
+        if not password:
+            raise ValueError("Users must have password")
+        if not role:
+            raise ValueError("Users must have role")
+        user = self.model(
+            email=self.normalize_email(email),
+            username=username,
+            role=role,
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
