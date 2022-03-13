2c3f0befd09e743cdf3573a568aa5f3021f48906
Bekmaganbetov Zhanbolat <180107180@stu.sdu.edu.kz>
2022-03-14 00:12:15 +0600

    feat(games): added filters for game and category

---
+++ Project/backend/games_platform/games/filters.py
@@ -0,0 +1,38 @@
+from django_filters import rest_framework as filters
+from .models import Game, Category
+
+
+class CategoryFilter(filters.FilterSet):
+    icon = filters.CharFilter(
+        field_name="icon",
+        lookup_expr="iexact",
+    )
+    name = filters.CharFilter(
+        field_name="name",
+        lookup_expr="iexact",
+    )
+
+    class Meta:
+        model = Category
+        fields = [
+            "icon",
+            "name",
+        ]
+
+
+class GameFilter(filters.FilterSet):
+    title = filters.CharFilter(
+        field_name="title",
+        lookup_expr="iexact",
+    )
+    description = filters.CharFilter(
+        field_name="description",
+        lookup_expr="icontains",
+    )
+
+    class Meta:
+        model = Game
+        fields = [
+            "title",
+            "description",
+        ]
--- Project/backend/games_platform/games/views.py
+++ Project/backend/games_platform/games/views.py
@@ -1,17 +1,24 @@
+from typing import Tuple
 from django.shortcuts import render
+from django_filters import rest_framework as filters
 from rest_framework import viewsets
+
 from .models import Category, Game
 from .serializers import CategorySerializer, GameSerializer
-from typing import Tuple
+from .filters import CategoryFilter, GameFilter
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
     http_method_names: Tuple = ("get", "post", "patch", "delete")
     serializer_class = CategorySerializer
     queryset = Category.objects.filter(deleted_at=None)
+    filter_backends = (filters.DjangoFilterBackend,)
+    filterset_class = CategoryFilter
 
 
 class GameViewSet(viewsets.ModelViewSet):
     http_method_names: Tuple = ("get", "post", "patch", "delete")
     serializer_class = GameSerializer
     queryset = Game.objects.filter(deleted_at=None)
+    filter_backends = (filters.DjangoFilterBackend,)
+    filterset_class = GameFilter
