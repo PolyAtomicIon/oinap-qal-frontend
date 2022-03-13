bf09e47035f6d7abf4ab73804a6129f75ae61621
Bekmaganbetov Zhanbolat <180107180@stu.sdu.edu.kz>
2022-03-13 22:10:57 +0600

    refactor(games): updated Game and Category models

--- Project/backend/games_platform/games/models.py
+++ Project/backend/games_platform/games/models.py
@@ -1,19 +1,26 @@
 from django.db import models
+from core.models import CommonModel
 from .choices import rating_choices
 
 
-class Category(models.Model):
+class Category(CommonModel):
     icon = models.CharField(
         verbose_name="icon",
         max_length=35,
+        null=True,
+        blank=True,
     )
     name = models.CharField(
         verbose_name="name",
         max_length=100,
     )
 
+    class Meta:
+        verbose_name = "category"
+        verbose_name_plural = "categories"
 
-class Game(models.Model):
+
+class Game(CommonModel):
     title = models.CharField(
         verbose_name="title",
         max_length=255,
@@ -30,7 +37,13 @@ class Game(models.Model):
     )
     views = models.BigIntegerField(
         verbose_name="views_count",
+        default=0,
     )
 
     def __str__(self):
         return self.title
+    
+
+    class Meta:
+        verbose_name = "game"
+        verbose_name_plural = "games"
