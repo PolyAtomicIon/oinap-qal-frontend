0ea5a4845186ded6ec8df6f3adc3e1e4a31a2d5d
Bekzat Yernat <gimtimes@gmail.com>
2022-04-18 03:43:02 +0600

    feat(user): make responsive on mobile

--- Project/frontend/src/components/molecules/UserProfile.vue
+++ Project/frontend/src/components/molecules/UserProfile.vue
@@ -1,15 +1,19 @@
 <template>
-  <q-card flat class="user-profile text-white q-px-lg q-py-lg">
-    <q-card-section horizontal>
+  <div class="text-white q-px-lg q-py-lg">
+    <div
+      class="user-profile"
+    >
       <q-img
         :src="require('src/assets/user/'+userAbout.img)"
         width="160px"
         height="160px"
       ></q-img>
 
-      <q-card-section vertical class="justify-around q-ml-md">
+      <div
+        class="user-profile__rank justify-around q-ml-md"
+      >
         <h1 class="text-h1">{{userAbout.name}}</h1>
-        <div class="q-mt-lg">
+        <div class="user-profile__rating">
           <img
             src="~/assets/icons/crown.svg"
             alt="crown-icon"
@@ -17,11 +21,11 @@
             height="20"
             width="24"
           >
-          <span class="user-profile__rating">{{userAbout.rating}}</span>
+          <span>{{userAbout.rating}}</span>
         </div>
-      </q-card-section>
-    </q-card-section>
-  </q-card>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -43,8 +47,21 @@ export default {
 
 <style lang="scss">
 .user-profile{
-  background: transparent;
+  display: flex;
+  gap: 24px;
+  @media screen and (max-width: $breakpoint-sm) {
+    flex-direction: column;
+    align-items: center;
+  }
+  &__rank {
+    display: flex;
+    flex-direction: column;
+    @media screen and (max-width: $breakpoint-sm) {
+      align-items: center;
+    }
+  }
   &__rating {
+    margin-top: 0px;
     text-transform: uppercase;
     font-size: 24px;
     font-weight: 300;
