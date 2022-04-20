a40c3f0b66fd688b1cc096aaaca32642a4bf9e97
Abyl239 <altajanovabylay@gmail.com>
2022-04-20 20:33:47 +0600

    footer

---
+++ Project/frontend/src/components/molecules/AppFooter.vue
@@ -0,0 +1,45 @@
+<template>
+  <div
+    class="footer desktop-only bg-transparent"
+  >
+    <q-separator color="dark-grey"/>
+    <div class="footer__container">
+      <div class="footer q-pa-lg">
+        <div
+          class="footer__title"
+          @click="$router.push('/home')"
+        >
+          <span>@2022 Graduaders</span>
+        </div>
+        <div
+          class="footer__links flex justify-between q-pt-sm"
+        >
+          <span>Пользовательское соглашение</span>
+          <span>Политика конфиденциальности</span>
+          <span>Техническая поддержка</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+  name: 'AppFooter',
+});
+</script>
+<style lang="scss">
+.footer{
+  &__container{
+    width: 650px;
+  }
+  &__title{
+    color: $dark-grey;
+  }
+  &__links{
+    color: $purple;
+    cursor: pointer;
+  }
+}
+</style>
--- Project/frontend/src/pages/Games/index.vue
+++ Project/frontend/src/pages/Games/index.vue
@@ -1,18 +1,23 @@
 <template>
-  <q-page class="q-pa-md">
+  <q-page class="flex column justify-between">
+    <div class="q-pa-md">
     <my-banner></my-banner>
     <router-view></router-view>
+    </div>
+    <app-footer/>
   </q-page>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import MyBanner from '../../components/molecules/Banner.vue';
+import AppFooter from '../../components/molecules/AppFooter.vue';
 
 export default defineComponent({
   name: 'PageIndex',
   components: {
     MyBanner,
+    AppFooter,
   },
   setup() {
     return {
